48249c545e683553d95938091c214c6d572b908e
unknown <jacobkorede5@gmail.com>
2023-08-29 13:19:08 +0100

    updated urls

--- server/src/server.ts
+++ server/src/server.ts
@@ -7,8 +7,10 @@ import { getQuotes } from './requests/quotesrequests'
 
 const app:Express=express()
 
-const server=app.listen(3000,()=>{
-    console.log("listening on port 3000")
+const PORT=process.env.PORT || 3000
+
+const server=app.listen(PORT,()=>{
+    console.log(`listening on port ${PORT}`)
 })
 
 
